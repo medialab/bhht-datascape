efd35f95d09a883adf54e700c26c366d10529402
Yomguithereal <guillaumeplique@gmail.com>
2017-03-13 12:03:48 +0100

    Adding helper

--- scripts/build-indices.js
+++ scripts/build-indices.js
@@ -23,7 +23,7 @@ require('util').inspect.defaultOptions.colors = true;
  * Constants.
  */
 const BASE_PEOPLE = 'base1_individus.csv',
-      BASE_LOCATIONS = 'base2_locations.csv';
+      BASE_LOCATIONS = 'base2_locations.csv',
       BASE_PATHS = 'base3_trajectoires.csv';
 
 const MAPPINGS = require('../specs/mappings.json'),
