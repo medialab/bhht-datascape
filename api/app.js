4e065ce93faead11d0ecf936b1b9d535ccf6b405
Yomguithereal <guillaumeplique@gmail.com>
2017-03-14 11:05:08 +0100

    Drafting server

---
+++ api/app.js
@@ -0,0 +1,29 @@
+/**
+ * BHHT Datascape Express App
+ * ===========================
+ *
+ * Describing the Express app serving the API.
+ */
+const express = require('express'),
+      wrap = require('dolman'),
+      bodyParser = require('body-parser'),
+      compress = require('compression');
+
+/**
+ * Describing the application.
+ */
+const app = express();
+
+app.use(bodyParser.urlencoded({limit: '5mb', extended: true}));
+app.use(bodyParser.json({limit: '5mb'}));
+app.use(compress());
+
+/**
+ * Mounting routers.
+ */
+app.get('/hello', (req, res) => res.json({hello: 'world'}));
+
+/**
+ * Exporting the application.
+ */
+module.exports = app;
---
+++ scripts/api.js
@@ -0,0 +1,9 @@
+/**
+ * BHHT Datascape API Start Script
+ * ================================
+ *
+ * Script starting the API.
+ */
+const app = require('../api/app.js');
+
+app.listen(4000);
