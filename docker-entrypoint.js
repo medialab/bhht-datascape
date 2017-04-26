9450c5af0f4d68e51724ccb9a1f12d611c4f3eea
jri-sp <jeremy.richard@sciences-po.fr>
2017-04-26 15:30:32 +0200

    Fix function name

--- docker-entrypoint.js
+++ docker-entrypoint.js
@@ -1,17 +1,17 @@
 const fs = require('fs'),
       config = require('./config.example.json');
 
-function getEnv(coerce, name, defaultValue) {
+function getEnv(coerce, name, defaultvalue) {
   const result = process.env[name];
 
   return result ? coerce(result) : defaultvalue;
 }
 
-config.api.port = config(Number, 'API_PORT', 4000);
-config.elasticsearch.host = config(String, 'ES_HOST', 'index');
-config.elasticsearch.port = config(Number, 'ES_PORT', 9200);
-config.elasticsearch.user = config(String, 'ES_USER', 'user');
-config.elasticsearch.user = config(String, 'ES_PASSWORD', 'password');
+config.api.port = getEnv(Number, 'API_PORT', 4000);
+config.elasticsearch.host = getEnv(String, 'ES_HOST', 'index');
+config.elasticsearch.port = getEnv(Number, 'ES_PORT', 9200);
+config.elasticsearch.user = getEnv(String, 'ES_USER', 'user');
+config.elasticsearch.user = getEnv(String, 'ES_PASSWORD', 'password');
 
 fs.writeFileSync('./config.json', JSON.stringify(config, null, 2));
 
