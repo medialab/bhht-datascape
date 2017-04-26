4819ee27241fa61ea6dbd3d27c77360089633632
Yomguithereal <guillaumeplique@gmail.com>
2017-04-26 15:21:03 +0200

    Rewriting docker endpoint

--- docker-entrypoint.js
+++ docker-entrypoint.js
@@ -1,22 +1,18 @@
-var fs = require('fs');
-var file = require('./config.example.json');
+const fs = require('fs'),
+      config = require('./config.example.json');
 
-function GetEnvironmentVar(varname, defaultvalue)
-{
-    var result = process.env[varname];
-    if (result)
-        return result;
-    else
-        return defaultvalue;
+function getEnv(coerce, name, defaultValue) {
+  const result = process.env[name];
+
+  return result ? coerce(result) : defaultvalue;
 }
 
-file.api.port = GetEnvironmentVar("API_PORT", 4000);
-file.elasticsearch.host = GetEnvironmentVar("ES_HOST", "index");
-file.elasticsearch.port = GetEnvironmentVar("ES_PORT", 9200);
-file.elasticsearch.user = GetEnvironmentVar("ES_USER", "user");
-file.elasticsearch.user = GetEnvironmentVar("ES_PASSWORD", "password");
+config.api.port = config(Number, 'API_PORT', 4000);
+config.elasticsearch.host = config(String, 'ES_HOST', 'index');
+config.elasticsearch.port = config(Number, 'ES_PORT', 9200);
+config.elasticsearch.user = config(String, 'ES_USER', 'user');
+config.elasticsearch.user = config(String, 'ES_PASSWORD', 'password');
 
-fs.writeFileSync('./config.json', JSON.stringify(file, null, 2));
+fs.writeFileSync('./config.json', JSON.stringify(config, null, 2));
 
-var childProcess = require("child_process");
-childProcess.fork("./scripts/api.js");
+require('./scripts/api.js');
