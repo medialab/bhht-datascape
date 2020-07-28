0383b8c520bcbdd60ea6dd3d7e6a4653b78b9a53
Yomguithereal <guillaumeplique@gmail.com>
2020-07-28 14:24:15 +0200

    Names dict decoding

--- src/assets.js
+++ src/assets.js
@@ -1,23 +1,81 @@
 import Papa from 'papaparse';
 import {inflate} from 'pako';
+import urljoin from 'url-join';
+import split from 'obliterator/split';
 
-console.log(BASE_URL);
+const DEFAULT_PARSE_OPTIONS = {
+  header: true,
+  skipEmptyLines: true,
+  dynamicTyping: true
+};
 
-// Papa.parse('http://localhost:3000/top.csv.gz', {
-//   download: true,
-//   complete(results) {
-//     console.log(results);
-//   }
-// });
+function get(url, callback) {
+  const complete = results => {
+    if (results.errors.length !== 0) return callback(results.errors);
 
-console.time('gz')
-fetch('http://localhost:3000/names.txt.gz')
-  .then(response => response.arrayBuffer())
-  .then(buffer => {
-    const string = inflate(buffer, {to: 'string'});
+    return callback(null, results.data);
+  };
 
-    Papa.parse(string, {fastMode: true, complete: results => {
-      console.timeEnd('gz');
-      console.log(results);
-    }});
+  if (url.endsWith('.gz')) {
+    return fetch(url)
+      .then(response => response.arrayBuffer())
+      .then(buffer => {
+        const string = inflate(buffer, {to: 'string'});
+
+        Papa.parse(string, {
+          ...DEFAULT_PARSE_OPTIONS,
+          complete
+        });
+      });
+  }
+
+  return Papa.parse(url, {
+    ...DEFAULT_PARSE_OPTIONS,
+    download: true,
+    complete
   });
+}
+
+const DECODER = /(^\d+)/;
+const UNDERSCORE = /_/g;
+
+function decodeName(previous, entry) {
+  let offset, suffix, s;
+
+  if (entry.includes('§')) {
+    s = entry.split('§');
+    offset = +s[0];
+    suffix = s[1];
+  } else {
+    s = entry.split(DECODER);
+    offset = +s[1];
+    suffix = s[2];
+  }
+
+  return (previous.slice(0, offset) + suffix).replace(UNDERSCORE, ' ');
+}
+
+function getNames(callback) {
+  console.time('names');
+
+  return fetch(urljoin(BASE_URL, 'names.txt.gz'))
+    .then(response => response.arrayBuffer())
+    .then(buffer => {
+      const string = inflate(buffer, {to: 'string'});
+
+      const names = [];
+
+      let previous = '';
+
+      for (const entry of split(/\n/g, string)) {
+        const decoded = decodeName(previous, entry);
+
+        names.push(decoded);
+
+        previous = decoded;
+      }
+
+      console.timeEnd('names');
+      return callback(null, names);
+    });
+}
--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
-import NavBar from './NavBar';
+
+const titleStyle = {
+  textAlign: 'center',
+  padding: '15px'
+};
 
 export default function App() {
   return (
-    <NavBar />
+    <div className="container">
+      <div className="columns">
+        <div className="column is-2" />
+        <div className="column is-8">
+          <div className="content">
+            <h1 style={titleStyle}>A Brief History of Human Time</h1>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
