01b4ecbecfc6ee0d877411b0099c62fdddeddcab
Yomguithereal <guillaumeplique@gmail.com>
2021-03-01 20:03:09 +0100

    Adding figure files

--- scripts/make.sh
+++ scripts/make.sh
@@ -4,8 +4,9 @@ npm run clean
 # Bulding javascript
 npm run build
 
-# Copying data
+# Copying data & images
 cp -r ./data build/data
+cp -r ./img build/img
 
 # Templating index
 sed s@/build/bundle.js@$BASE_URL/bundle.js@ index.html > build/index.html
